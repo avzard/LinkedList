83b637e1da89af4b80c0a3196ab5f67f2371d0ef
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-23 14:55:37 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 namespace LinkedList
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Welcome_To_LinkedList***");
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("\nAfter Inserting Element...");
+            list.InserAtParticularPosition(2, 30);
+            list.Display();
+            Console.ReadLine();
         }
     }
 }
