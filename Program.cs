69ee7d8fcffb9d96be3124c0d92b588f489f0e60
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-23 10:50:00 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+
+using System;
 
 namespace LinkedList
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Welcome_To_LinkedList***");
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
